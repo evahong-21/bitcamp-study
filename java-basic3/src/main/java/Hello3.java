7115fdb4c85a377539ba6b7504af68161e08a088
evahong-21 <you@example.com>
2021-06-30 17:40:34 +0900

    ignore

--- java-basic3/src/main/java/Hello3.java
+++
@@ -1,8 +0,0 @@
-class Hello3 {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        System.out.println("안녕하세요");
-        System.out.println("3");
-    }
-    
-}
