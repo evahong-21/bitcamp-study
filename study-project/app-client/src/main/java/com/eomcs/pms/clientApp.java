24015ec55c0ed316779ff3756db61e9a7c7a05c3
unknown <evahong94@gmail.com>
2021-09-27 12:41:02 +0900

    'server_client'

--- study-project/app-client/src/main/java/com/eomcs/pms/clientApp.java
+++ study-project/app-client/src/main/java/com/eomcs/pms/clientApp.java
@@ -1,7 +1,40 @@
 package com.eomcs.pms;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class clientApp {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws UnknownHostException, IOException {
     System.out.println("client");
+
+    System.out.println("1) 소켓 준비");
+    Socket socket = new Socket("127.0.0.1",8888); // 서버와 접속이 이루어지면 리턴
+
+    System.out.println("2) 서버와 연결 되었음.");
+
+    System.out.println("입출력 객체 준비");
+
+    PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+    System.out.println("데이터보내기");
+    out.println("Hello!");
+    out.flush();
+
+    System.out.println("서버가 보낸 데이터 확인");
+    String result = in.readLine();
+    System.out.println(">>> " + result);
+
+    System.out.println("3) 서버와의 연결을 끊음");
+    in.close();
+    out.close();
+    socket.close();
+
+
   }
 }
--- study-project/app-server/src/main/java/com/eomcs/pms/serverApp.java
+++ study-project/app-server/src/main/java/com/eomcs/pms/serverApp.java
@@ -1,7 +1,42 @@
 package com.eomcs.pms;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class serverApp {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     System.out.println("server");
+
+    System.out.println("1) 서버소캣준비");
+    ServerSocket serverSocket = new ServerSocket(8888);
+
+    System.out.println("2) 클라이언트의 접속을 기다림");
+    Socket socket = serverSocket.accept();
+
+    System.out.println("3) 클라이언트가 접속했음");
+
+    System.out.println("클라이언트와 데이터를 주고받기 위해 입출력 스트림 준비");
+    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+    PrintWriter out = new PrintWriter(socket.getOutputStream());
+
+    System.out.println("클라이언트에게 데이터 보내기");
+    out.println("안녕");
+    //데이터를 확실하게 보내고 싶으면 flush를 명시하자. close에 의존하지 말자
+    out.flush();
+
+    String message = in.readLine();
+    System.out.println("===> "+message);
+    System.out.println("4) 클라이언트와의 접속을 끊음");
+    out.close();
+    in.close();
+    socket.close();
+
+    System.out.println("5) 서버 소켓 종료");
+    serverSocket.close();
   }
 }
